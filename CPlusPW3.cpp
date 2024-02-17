ce628f269fcacffcef0df617f9231712200b484a
zeshalondrag <isip_a.v.samarskiy@mpt.ru>
2024-02-17 11:10:23 +0300

    Двумерный массив

---
+++ CPlusPW3.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <string>
+#include <Windows.h>
+
+using namespace std;
+
+int main()
+{
+    SetConsoleOutputCP(1251);
+    setlocale(0, "");
+
+    const int rows = 3, columns = 5;
+    int numbers[rows][columns];
+
+    cout << "Введите элементы массива " << rows << "x" << columns << ":\n";
+
+    for (int i = 0; i < rows; i++) // Ввод чисел в двумерный массив
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            cin >> numbers[i][j];
+        }
+    }
+    cout << endl;
+
+    for (int i = 0; i < rows; i++) // Вывод на экран двумерного массива
+    {
+        int sum = 0;
+
+        for (int j = 0; j < columns; j++)
+        {
+            cout << numbers[i][j] << "\t";
+            sum += numbers[i][j];
+        }
+
+        double avg = (double)sum / columns;
+        cout << "\tСреднее арифметическое: " << avg << endl;
+    }
+    return 0;
+}
